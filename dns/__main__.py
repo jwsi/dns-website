178c84d6d09da1aca3dfee763a5c4e19c8c200bb
James Webb <james@ultra-horizon.com>
2018-11-06 21:07:16 +0000

    Working DNS replies now.

--- dns/__main__.py
+++ dns/__main__.py
@@ -1,35 +1,38 @@
-import socketserver, dnslib, binascii
-from threading import Thread
-from time import sleep
+import threading, socket, logging, dnslib
+
+class UDPHandler():
+
+    def __init__(self):
+        logging.info("Initializing UDP Handler")
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(("0.0.0.0", 8000))
+        self.clients_list = []
 
 
-def threaded_handler(datagram):
-    request = dnslib.DNSRecord.parse(datagram)
-    print(request)
-    print(request.questions)
-    for question in request.questions:
-        print("Record print")
-        print(question.qname)
-        print(dnslib.QTYPE[question.qtype])
-        # Do some search on these params.
-    sleep(5)
+    def respond_to_client(self, datagram, ip):
+        request = dnslib.DNSRecord.parse(datagram)
+        recursion_desired = request.header.rd
+        id = request.header.id
+        for question in request.questions:
+            domain = question.qname
+            record_type = dnslib.QTYPE[question.qtype]
+            response = dnslib.DNSRecord(dnslib.DNSHeader(id=id, qr=1, aa=1, ra=0, rd=recursion_desired),
+                                        questions=[dnslib.DNSQuestion(domain)],
+                                        rr=[dnslib.RR(domain,rdata=dnslib.A("1.2.3.4"))],
+                                        auth = [dnslib.RR(domain, rdata=dnslib.A("1.2.3.4"))])
+            self.sock.sendto(response.pack(), ip)
+            import time
+            time.sleep(10)
 
 
-class MyUDPHandler(socketserver.BaseRequestHandler):
-    """
-    This class works similar to the TCP handler class, except that
-    self.request consists of a pair of data and client socket, and since
-    there is no connection the client address must be given explicitly
-    when sending data back via sendto().
-    """
-    def handle(self):
-        data = self.request[0].strip()
-        socket = self.request[1]
-        thread = Thread(target=threaded_handler, args=(data, ))
-        thread.start()
-        # socket.sendto(data.upper(), self.client_address)
+    def listen(self):
+        while True:
+            question, client = self.sock.recvfrom(1024)
+            t = threading.Thread(target=self.respond_to_client, args=(question, client,))
+            t.start()
 
-
-if __name__ == "__main__":
-    server = socketserver.UDPServer(("0.0.0.0", 8000), MyUDPHandler)
-    server.serve_forever()
+if __name__ == '__main__':
+    # Make sure all log messages show up
+    logging.getLogger().setLevel(logging.DEBUG)
+    handler = UDPHandler()
+    handler.listen()
