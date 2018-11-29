ef8b1741f83f8b346a09d92710d2c3ee3c5b5c75
James Webb <james@ultra-horizon.com>
2018-11-29 21:08:38 +0000

    Add working authority checking system.

--- api/endpoints/check.py
+++ api/endpoints/check.py
@@ -2,7 +2,7 @@ from flask import jsonify
 from supporting.dynamodb import records, Attr
 from classes.errors import ControlledException
 from classes.status import ReturnCode
-import dns.resolver
+import whois
 
 
 def PUT_check(domain, user):
@@ -11,8 +11,29 @@ def PUT_check(domain, user):
     :param domain: Domain to check.
     :param user: Associated user.
     """
+    record = _get_record(domain, user)
+    correct_ns = {"ns1.uh-dns.com", "ns2.uh-dns.com", "ns3.uh-dns.com", "ns4.uh-dns.com"}
+    queried_ns = _get_whois_nameservers(domain)
+    if correct_ns != queried_ns:
+        raise ControlledException(ReturnCode.CHECK_FAILED)
+    _check_no_live_domain(domain)
+    # If successful - set the record to be live.
+    record["live"] = True
+    records.put_item(Item=record)
+    return jsonify({
+        "status" : ReturnCode.SUCCESS
+    })
+
+
+def _get_record(domain, user):
+    """
+    Gets a record object from dynamoDB
+    :param domain: domain to query.
+    :param user: user to query.
+    :return: record object.
+    """
     try:
-        record = records.get_item(
+        return records.get_item(
             Key={
                 "domain": domain,
                 "user_id" : user
@@ -20,16 +41,27 @@ def PUT_check(domain, user):
         )["Item"]
     except KeyError: # Catch records that don't exist.
         raise ControlledException(ReturnCode.DOMAIN_NOT_FOUND)
-    correct_ns = {"ns1.ultra-horizon.com", "ns2.ultra-horizon.com"}
-    queried_ns = set()
+
+
+def _get_whois_nameservers(domain):
+    """
+    Gets the authorative nameservers for a domain.
+    :param domain: domain to check.
+    :return: Authorative nameservers for a domain.
+    """
     try:
-        answers = dns.resolver.query(record["domain"], 'NS')
-    except: # No nameservers present.
-        answers = []
-    for rdata in answers:
-        queried_ns.add(rdata.to_text())
-    if correct_ns != queried_ns:
-        raise ControlledException(ReturnCode.CHECK_FAILED)
+        response = whois.whois(domain[:-1]) # Do WHOIS lookup without trailing .
+        ns = {record.lower() for record in response.name_servers}
+    except: # No nameservers present
+        ns = []
+    return ns
+
+
+def _check_no_live_domain(domain):
+    """
+    Check that no live domain exists already.
+    :param domain: domain to check.
+    """
     try:
         already_live = records.scan(
             FilterExpression=Attr('domain').eq(domain) & Attr('live').eq(True)
@@ -38,9 +70,3 @@ def PUT_check(domain, user):
         already_live = []
     if len(already_live) > 0:
         raise ControlledException(ReturnCode.DOMAIN_ALREADY_LIVE)
-    # If successful - set the record to be live.
-    record.live = True
-    records.put_item(Item=record)
-    return jsonify({
-        "status" : ReturnCode.SUCCESS
-    })
