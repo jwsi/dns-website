b3ca36912eca04fd0f7967f487875df2ebf7962b
rubenwardy <rw@rubenwardy.com>
2018-12-03 13:41:02 +0000

    Move remaining endpoint functions to submodule to be consistent

---
+++ api/endpoints/records.py
@@ -0,0 +1,91 @@
+from flask import jsonify, abort
+from supporting.dynamodb import records, Attr
+
+
+optstr = "OPTSTR"
+
+ENTRY_TYPES = {
+    "A":     { "ttl": int, "value": [str] },
+    "AAAA":  { "ttl": int, "value": [str] },
+    "CAA":   { "ttl": int, "value": [{"flags": int, "tag": str, "value": str }]},
+    "CNAME": { "ttl": int, "domain": str },
+    "MX":    { "ttl": int, "value": [{ "domain": str, "preference": int }] },
+    "NAPTR": { "ttl": int, "value": [{ "order": int, "preference": int, "flags": str, "service": str, "regexp": optstr, "replacement": str }]},
+    "NS":    { "ttl": int, "value": [str] },
+    "SOA":   { "ttl": int, "times": [int], "mname": str, "rname": str },
+    "SRV":   { "ttl": int, "value": [{ "priority": int, "weight": int, "port": int, "target": str }] },
+    "TXT":   { "ttl": int, "value": str },
+}
+
+
+def check_structure(value, schema):
+    if schema == optstr:
+        return value is None or isinstance(value, str)
+    elif isinstance(schema, dict) and isinstance(value, dict):
+        return all(k in value and check_structure(value[k], schema[k]) for k in schema)
+    elif isinstance(schema, list) and isinstance(value, list):
+        return all(check_structure(c, schema[0]) for c in value)
+    elif isinstance(schema, type):
+        return isinstance(value, schema)
+    else:
+        return False
+
+
+def check_stucture_for_type(struct, type):
+    return check_structure(struct, ENTRY_TYPES[type])
+
+
+def GET_records(user_id):
+    resp = records.scan(FilterExpression=Attr("user_id").eq(user_id))
+    return jsonify([{ "domain": r["domain"], "live": r["live"] } for r in resp["Items"]])
+
+
+def GET_record(domain, user_id):
+    response = records.get_item(Key={
+            "domain": domain,
+            "user_id": user_id,
+        })
+
+    if "Item" not in response:
+        abort(404)
+
+    item = response["Item"]
+    return jsonify(item)
+
+
+def GET_record_entry(user_id, domain, type):
+    if type not in ENTRY_TYPES:
+        abort(404)
+
+    response = records.get_item(Key={
+            "domain": domain,
+            "user_id": user_id
+        })
+
+    if "Item" not in response:
+        abort(404)
+
+    item = response["Item"]
+    return jsonify(item[type])
+
+
+def PUT_record_entry(user_id, domain, type, json):
+    if type not in ENTRY_TYPES:
+        abort(404)
+
+    response = records.get_item(Key={
+            "domain": domain,
+            "user_id": user_id
+        })
+
+    if "Item" not in response:
+        abort(404)
+
+    item = response["Item"]
+
+    item[type] = json
+    if not check_stucture_for_type(item[type], type):
+        abort(400)
+
+    ret = records.put_item(Item=item)
+    return jsonify(ret)
--- api/main.py
+++ api/main.py
@@ -1,45 +1,13 @@
-from flask import Flask, jsonify, abort, url_for, request
-from supporting.dynamodb import records as t_records, Attr
+from flask import Flask, jsonify, request
 
 from endpoints.check import PUT_check
+from endpoints.records import GET_records, GET_record, GET_record_entry, PUT_record_entry
 from classes.errors import ControlledException
 
 # Define the Flask name as "app".
 # Note: EB accepts only "application" so point "app" to this for short hand.
 application = Flask(__name__)
 app = application
-
-optstr = "OPTSTR"
-
-ENTRY_TYPES = {
-    "A":     { "ttl": int, "value": [str] },
-    "AAAA":  { "ttl": int, "value": [str] },
-    "CAA":   { "ttl": int, "value": [{"flags": int, "tag": str, "value": str }]},
-    "CNAME": { "ttl": int, "domain": str },
-    "MX":    { "ttl": int, "value": [{ "domain": str, "preference": int }] },
-    "NAPTR": { "ttl": int, "value": [{ "order": int, "preference": int, "flags": str, "service": str, "regexp": optstr, "replacement": str }]},
-    "NS":    { "ttl": int, "value": [str] },
-    "SOA":   { "ttl": int, "times": [int], "mname": str, "rname": str },
-    "SRV":   { "ttl": int, "value": [{ "priority": int, "weight": int, "port": int, "target": str }] },
-    "TXT":   { "ttl": int, "value": str },
-}
-
-
-def check_structure(value, schema):
-    if schema == optstr:
-        return value is None or isinstance(value, str)
-    elif isinstance(schema, dict) and isinstance(value, dict):
-        return all(k in value and check_structure(value[k], schema[k]) for k in schema)
-    elif isinstance(schema, list) and isinstance(value, list):
-        return all(check_structure(c, schema[0]) for c in value)
-    elif isinstance(schema, type):
-        return isinstance(value, schema)
-    else:
-        return False
-
-
-def check_stucture_for_type(struct, type):
-    return check_structure(struct, ENTRY_TYPES[type])
 
 
 class User:
@@ -51,48 +19,20 @@ current_user = User()
 
 @app.route("/r/")
 def records():
-    resp = t_records.scan(FilterExpression=Attr("user_id").eq(current_user.user_id))
-    return jsonify([{ "domain": r["domain"], "live": r["live"] } for r in resp["Items"]])
+    return GET_records(current_user.user_id)
 
 
 @app.route("/r/<domain>/")
 def record(domain):
-    response = t_records.get_item(Key={
-            "domain": domain,
-            "user_id": current_user.user_id,
-        })
-
-    if "Item" not in response:
-        abort(404)
-
-    item = response["Item"]
-    return jsonify(item)
+    return GET_record(domain, current_user.user_id)
 
 
 @app.route("/r/<domain>/<type>/", methods=["GET", "PUT"])
 def record_entry(domain, type):
-    if type not in ENTRY_TYPES:
-        abort(404)
-
-    response = t_records.get_item(Key={
-            "domain": domain,
-            "user_id": current_user.user_id
-        })
-
-    if "Item" not in response:
-        abort(404)
-
-    item = response["Item"]
     if request.method == "GET":
-        return jsonify(item[type])
-
+        return GET_record_entry(current_user.user_id, domain, type)
     else:
-        item[type] = request.get_json()
-        if not check_stucture_for_type(item[type], type):
-            abort(400)
-
-        ret = t_records.put_item(Item=item)
-        return jsonify(ret)
+        return PUT_record_entry(current_user.user_id, domain, type, request.get_json())
 
 
 @app.route("/r/<domain>/check/", methods=["PUT"])
