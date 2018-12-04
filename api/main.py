2f3a0fb4a0f9e097614e2d3c03d386ef4fae2048
rubenwardy <rw@rubenwardy.com>
2018-12-04 14:42:02 +0000

    Add dummy frontend using bootstrap

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
+import flask_menu as menu
 from endpoints.check import PUT_check
 from endpoints.records import GET_records, GET_record, GET_record_entry, PUT_record_entry
 from classes.errors import ControlledException
@@ -8,13 +9,44 @@ from classes.errors import ControlledException
 # Note: EB accepts only "application" so point "app" to this for short hand.
 application = Flask(__name__)
 app = application
+menu.Menu(app=app)
 
 
 class User:
-    user_id = "jw"
+    user_id  = "jw"
+    fullname = "James Webb"
+    is_authenticated = True
 
 
 current_user = User()
+
+
+@app.context_processor
+def inject_user():
+    return dict(current_user=current_user)
+
+
+def should_return_JSON():
+    return "application/json" in request.accept_mimetypes and \
+        not "text/html" in request.accept_mimetypes
+
+
+def split_endpoint(path):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        data = f(*args, **kwargs)
+        if should_return_JSON():
+            return jsonify(data)
+        else:
+            return render_template(path, data=data)
+
+    return decorated_function
+
+
+@app.route("/")
+@menu.register_menu(app, ".", "Home")
+def index():
+    return render_template("index.html")
 
 
 @app.route("/r/")
