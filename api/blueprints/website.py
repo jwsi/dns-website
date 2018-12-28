bfc284924c0cfa810b09039d3a140a600596fbdc
rubenwardy <rw@rubenwardy.com>
2018-12-28 17:17:02 +0000

    Add WIP add record

--- api/blueprints/website.py
+++ api/blueprints/website.py
@@ -1,9 +1,15 @@
 from flask import Blueprint, render_template, request, abort
 from api.decorators.authentication import requires_auth
 from api.classes.db import db
+from api.classes.recordtype import RecordType
 from api import menu, current_user
+from flask_wtf import FlaskForm
+from wtforms import *
+from wtforms.validators import InputRequired, Length
+
 
 website = Blueprint('website', __name__)
+
 
 def should_return_JSON():
     return "application/json" in request.accept_mimetypes and \
@@ -31,8 +37,30 @@ def site_records(domain):
         abort(404)
 
     def getRTypes(record):
-        from .api import ENTRY_TYPES
-        ret = [{ "name": key, "data": record[key]} for key in ENTRY_TYPES if key in record]
+        print(RecordType.choices())
+        ret = [{ "name": key[0].name, "data": record[key[0].name]} for key in RecordType.choices() if key[0].name in record]
         return ret
 
-    return render_template("records.html", records=records, getRTypes=getRTypes)
+    return render_template("records.html", records=records, getRTypes=getRTypes, domain=domain)
+
+
+class RecordForm(FlaskForm):
+    domain = StringField("Domain", [InputRequired(), Length(1, 40)])
+    type   = SelectField("Type", [InputRequired()], choices=RecordType.choices(), coerce=RecordType.coerce, default=RecordType.A)
+    ttl    = IntegerField("TTL", default=10)
+    value  = StringField("Value", [InputRequired(), Length(0, 1000)])
+    submit = SubmitField("Save")
+
+
+@website.route("/sites/<domain>/new/", methods=["GET", "POST"])
+@requires_auth("user")
+def site_records_new(domain):
+    form = RecordForm(formdata=request.form)
+
+    if request.method == "GET":
+        form.domain.data = domain
+    else:
+        res = form.type.data.build(form.ttl.data, form.value.data)
+        print(res)
+
+    return render_template("record_newedit.html", form=form)
---
+++ api/classes/recordtype.py
@@ -0,0 +1,104 @@
+import enum
+import re
+
+retype = type(re.compile("hello, world"))
+ip_reg = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+ip6_reg = str
+domain_reg = str
+
+
+optstr = "OPTSTR"
+
+
+class RecordType(enum.Enum):
+    A     = "A"
+    AAAA  = "AAAA (V6)"
+    CAA   = "CAA"
+    CNAME = "CNAME"
+    MX    = "MX"
+    NAPTR = "NAPTR"
+    NS    = "Name Servers (NS)"
+    SOA   = "SOA"
+    SRV   = "SRV"
+    TXT   = "TXT"
+
+    def __str__(self):
+        return self.name
+
+    def check_structure(self, struct):
+        def __check_structure(value, schema):
+            if schema == optstr:
+                return value is None or isinstance(value, str)
+            elif isinstance(schema, dict) and isinstance(value, dict):
+                return all(k in value and __check_structure(value[k], schema[k]) for k in schema)
+            elif isinstance(schema, list) and isinstance(value, list):
+                return all(__check_structure(c, schema[0]) for c in value)
+            elif isinstance(schema, retype):
+                return schema.match(value)
+            elif isinstance(schema, type):
+                return isinstance(value, schema)
+            else:
+                return False
+
+        ENTRY_TYPES = {
+            "A":     { "ttl": int, "value": [ip_reg] },
+            "AAAA":  { "ttl": int, "value": [ip6_reg] },
+            "CAA":   { "ttl": int, "value": [{"flags": int, "tag": str, "value": str }]},
+            "CNAME": { "ttl": int, "domain": domain_reg },
+            "MX":    { "ttl": int, "value": [{ "domain": domain_reg, "preference": int }] },
+            "NAPTR": { "ttl": int, "value": [{ "order": int, "preference": int, "flags": str, "service": str, "regexp": optstr, "replacement": str }]},
+            "NS":    { "ttl": int, "value": [domain_reg] },
+            "SOA":   { "ttl": int, "times": [int], "mname": str, "rname": str },
+            "SRV":   { "ttl": int, "value": [{ "priority": int, "weight": int, "port": int, "target": str }] },
+            "TXT":   { "ttl": int, "value": str },
+        }
+        return __check_structure(struct, ENTRY_TYPES[self.name])
+
+    def build(self, ttl, value):
+        res = {
+            "ttl": int(ttl),
+        }
+
+        if self.name == "A" or self.name == "AAAA" or self.name == "NS":
+            res["value"] = [x.strip() for x in value.split(",")]
+        elif self.name == "CNAME":
+            res["domain"] = value.strip()
+        elif self.name == "TXT":
+            res["value"] = value.strip()
+        elif self.name == "CAA":
+            def splitRecord(x):
+                if len(x) != 3:
+                    return {}
+
+                return { "flags": int(x[0]), "tag": str(x[1]), "value": str(x[2]) }
+
+            res["value"] = [splitRecord(x.split("/")) for x in value.split(",")]
+        else:
+            print("Unknown build()")
+            return None
+
+        if not self.check_structure(res):
+            print("Malformed build()")
+            return None
+
+        return res
+
+
+    @classmethod
+    def check_stucture_for_type(struct, type):
+        return RecordType.get(type).check_structure(struct)
+
+    @classmethod
+    def get(cls, name):
+        try:
+            return RecordType[name.upper()]
+        except KeyError:
+            return None
+
+    @classmethod
+    def choices(cls):
+        return [(choice, choice.value) for choice in cls]
+
+    @classmethod
+    def coerce(cls, item):
+        return item if type(item) == RecordType else RecordType[item]
